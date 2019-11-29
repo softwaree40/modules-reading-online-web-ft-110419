4e057637e6eeb03c16eb3a29edf2c6da876ed7c2
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-29 17:14:22 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,7 +1,9 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
-class Kid 
-  
-  
-  
+class Kid
+include Dance
+attr_accessor :name
+  def initialize(name)
+  @name = name
+  end
 end
