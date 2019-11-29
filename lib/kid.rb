79f1d218fb23a2fccdc410b4fb7650ae0a2fb4c4
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-29 17:27:14 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,11 +1,11 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid
-    extend FancyDance::ClassMethods
+  extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
   extend MetaDancing
-include Dance
-attr_accessor :name
+  include Dance
+  attr_accessor :name
   def initialize(name)
   @name = name
   end
