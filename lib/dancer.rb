583cf10c778a3a8fd81a293dd087316eea3c4e50
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-29 17:15:07 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,11 +1,9 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 class Dancer
-      attr_accessor :name
-      include Dance
-     def initialize(name)
-         @name = name 
-         
-      end
-    
+  include Dance
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
 end
