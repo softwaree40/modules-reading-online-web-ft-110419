112041c9323a67c4241998775c04510dab74b0c3
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-29 17:01:08 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,5 +1,10 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 class Dancer
-    include Dance
+      include Dance
+     def initialize(name)
+         @name = name 
+         
+       end
+    
 end
