f534a69094f15fb61d1e9c144d3e6ab957f6bf55
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-30 02:59:18 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,9 +1,7 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 class Dancer
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethod
-  attr_accessor :name
+  attr_accessor:name
   def initialize(name)
     @name = name
   end
