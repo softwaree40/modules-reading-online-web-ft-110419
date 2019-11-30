93764223b753b169a9ed4bb38c1b01478a8605be
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-30 03:01:42 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 class Dancer
   include Dance 
-  extend
+  extend MetaDancing
   attr_accessor:name
   def initialize(name)
     @name = name
